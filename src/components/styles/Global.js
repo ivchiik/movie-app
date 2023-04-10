f0e01f0163fecf594151a0ca02d825f34c2b4d68
Iva <ivasaata@gmail.com>
2023-04-10 13:39:42 +0400

    new styles

--- src/components/styles/Global.js
+++ src/components/styles/Global.js
@@ -3,5 +3,8 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyles = createGlobalStyle`
     body{
         background: ${({ theme }) => theme.colors.body};
+        ::-webkit-scrollbar{
+            display: none;
+        }
     }
-`
+`;
--- src/components/styles/List.styled.js
+++ src/components/styles/List.styled.js
@@ -4,7 +4,26 @@ export const StyledList = styled.div`
   display: flex;
   flex-direction: row;
   overflow-x: auto;
+  overflow-y: hidden;
   flex-wrap: nowrap;
+
+  /* width */
+  ::-webkit-scrollbar {
+    width: 20px;
+    scrollbar-width: thin;
+  }
+
+  /* Track */
+  ::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 5px grey;
+    border-radius: 10px;
+  }
+
+  /* Handle */
+  ::-webkit-scrollbar-thumb {
+    background: rgb(229, 9, 20);
+    border-radius: 10px;
+  }
 `;
 
 export const StyledMovie = styled.div`
